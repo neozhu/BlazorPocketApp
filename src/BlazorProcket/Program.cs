f70d360e5bf73e5670cc49fbbe3378249f36d5f0
neozhu <new163@163.com>
2024-05-16 10:09:48 +0800

    ✔ add mudblazor component

--- src/BlazorProcket/Program.cs
+++ src/BlazorProcket/Program.cs
@@ -1,5 +1,11 @@
+using Blazored.LocalStorage;
 using BlazorProcket.Client.Pages;
+using BlazorProcket.Client.Services;
+using BlazorProcket.Client.Services.UserPreferences;
 using BlazorProcket.Components;
+using MudBlazor;
+using MudBlazor.Services;
+using MudExtensions.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,12 +14,52 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
+
+#region register MudBlazor.Services
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+
+    // we're currently planning on deprecating `PreventDuplicates`, at least to the end dev. however,
+    // we may end up wanting to instead set it as internal because the docs project relies on it
+    // to ensure that the Snackbar always allows duplicates. disabling the warning for now because
+    // the project is set to treat warnings as errors.
+#pragma warning disable 0618
+    config.SnackbarConfiguration.PreventDuplicates = false;
+#pragma warning restore 0618
+});
+builder.Services.AddMudPopoverService();
+builder.Services.AddMudBlazorSnackbar();
+builder.Services.AddMudBlazorDialog();
+builder.Services.AddMudExtensions();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<IUserPreferencesService, UserPreferencesService>();
+builder.Services.AddScoped<LayoutService>();
+#endregion
+
+
+
+
+#region register EndpointsApi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
+    // enable SwaggerUI
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 else
 {
