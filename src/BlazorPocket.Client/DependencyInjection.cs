cca39be40648abe3a0b91d516a20dab202f168c5
neozhu <new163@163.com>
2024-05-20 14:13:52 +0800

    fixed

--- src/BlazorPocket.Client/DependencyInjection.cs
+++ src/BlazorPocket.Client/DependencyInjection.cs
@@ -77,15 +77,15 @@ public static class DependencyInjection
 
     public static void TryAddProcketbaseServer(this IServiceCollection services,IConfiguration config)
     {
-        var appSettings = new AppSettings();
-        config.GetSection(AppSettings.KEY).Bind(appSettings);
+        var appSettings = new ApplicationSettings();
+        config.GetSection(ApplicationSettings.KEY).Bind(appSettings);
         services.AddSingleton(appSettings);
         services.AddScoped(s => new BlazorPocketApplication(appSettings.PocketbaseUrl, appSettings.AppName));
     }
     public static void TryAddProcketbaseWebAssembly(this IServiceCollection services, IConfiguration config)
     {
-        var appSettings = new AppSettings();
-        config.GetSection(AppSettings.KEY).Bind(appSettings);
+   
+        var appSettings= config.GetSection(ApplicationSettings.KEY).Get<ApplicationSettings>();
         services.AddSingleton(appSettings);
         services.AddSingleton(s => new BlazorPocketApplication(appSettings.PocketbaseUrl, appSettings.AppName));
     }
--- src/BlazorPocket.Client/Program.cs
+++ src/BlazorPocket.Client/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorPocket.Client;
+using BlazorPocket.Client.Configurations;
+using System.Net.Http.Json;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.Services.TryAddMudBlazorWebAssembly(builder.Configuration);
 builder.Services.TryAddProcketbaseWebAssembly(builder.Configuration);
 builder.Services.TrAddAuthenticationWebAssembly();
