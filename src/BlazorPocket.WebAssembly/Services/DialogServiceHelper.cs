f23d4ade9a054dd8c222916a0f35d2cd15869bea
hualin.zhu <neo.js.cn@gmail.com>
2024-06-15 13:02:27 +0800

    add languages resource

--- src/BlazorPocket.WebAssembly/Services/DialogServiceHelper.cs
+++ src/BlazorPocket.WebAssembly/Services/DialogServiceHelper.cs
@@ -3,6 +3,7 @@ using MudBlazor;
 
 namespace BlazorPocket.WebAssembly.Services;
 
+// This class represents a helper class for the dialog service.
 public class DialogServiceHelper
 {
     private readonly IDialogService _dialogService;
@@ -12,6 +13,13 @@ public class DialogServiceHelper
         _dialogService = dialogService;
     }
 
+    /// <summary>
+    /// Shows a confirmation dialog with the specified title and content text.
+    /// </summary>
+    /// <param name="title">The title of the confirmation dialog.</param>
+    /// <param name="contentText">The content text of the confirmation dialog.</param>
+    /// <param name="onConfirm">The action to be executed when the confirmation is confirmed.</param>
+    /// <param name="onCancel">The optional action to be executed when the confirmation is canceled.</param>
     public async Task ShowConfirmationDialog(string title, string contentText, Func<Task> onConfirm, Func<Task>? onCancel = null)
     {
         var parameters = new DialogParameters
