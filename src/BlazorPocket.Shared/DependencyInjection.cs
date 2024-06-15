b362300eaed3f481532f4d036ecfb473652f9123
hualin.zhu <neo.js.cn@gmail.com>
2024-06-15 13:26:36 +0800

    commit

--- src/BlazorPocket.Shared/DependencyInjection.cs
+++ src/BlazorPocket.Shared/DependencyInjection.cs
@@ -9,17 +9,12 @@ namespace BlazorPocket.Shared;
 public static class DependencyInjection
 {
  
-
     public static void TryAddProcketbase(this IServiceCollection services, IConfiguration config)
     {
-   
         var appSettings= config.GetSection(ApplicationSettings.KEY).Get<ApplicationSettings>();
         services.AddSingleton(appSettings);
         services.AddSingleton(s => new BlazorPocketApplication(appSettings.PocketbaseUrl, appSettings.AppName));
-         
     }
-
-   
     public static void TrAddAuthentication(this IServiceCollection services)
     {
         services.AddAuthorizationCore();
